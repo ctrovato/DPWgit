0c4c1f5a13deadfaedc7d29bd34ac9d57a4fda4a
ctrovato <carminetrovato@fullsail.edu>
2014-07-10 22:05:53 -0400

    Quiz one
    Started Function calc_area(w,h):

--- Trovato_Carmine_Quiz1/main.py
+++ Trovato_Carmine_Quiz1/main.py
@@ -4,4 +4,7 @@ Date: 07/10/14
 Quiz 1
 '''
 
-
+#function calc_area(w,h) takes the width and height
+def calc_area(w,h):
+    if w == h:
+        print "This is a square of: " + str(w * h) + " square feet, because the width and height are the same."
