26e68136954aae5b046f60df99a0d2739e57bdb0
ctrovato <carminetrovato@fullsail.edu>
2014-07-15 16:33:35 -0400

    Mailing list update CSS

--- MailingList/main.py
+++ MailingList/main.py
@@ -20,8 +20,28 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.GET:
             p.content = "First Name:" + self.request.GET['fname']
             p.content += "<br />Last Name:" + self.request.GET['lname']
-            p.content += "Newsletter:" + self.request.GET['marketing']
-            p.content += "" + self.request.GET['']
+            p.content += "<br />Email:" + self.request.GET['email']
+            p.content += "<br />Password:" + self.request.GET['pass']
+            p.content += "<br />Re-Password:" + self.request.GET['repass']
+            p.content += "<br /> Newsletter:" + self.request.GET['marketing']
+
+            p.content += "<br />Describe Yourself:"
+
+            if self.request.GET.has_key('artistic'):
+                p.content += self.request.GET['artistic'] + ', '
+
+            if self.request.GET.has_key('athletic'):
+                p.content += self.request.GET['athletic'] + ', '
+
+            if self.request.GET.has_key('artistic'):
+                p.content += self.request.GET['artistic'] + ', '
+
+            if self.request.GET.has_key('music'):
+                p.content += self.request.GET['music'] + ', '
+
+            if self.request.GET.has_key('food'):
+                p.content += self.request.GET['food'] + ', '
+
             self.response.write(p.print_out_page())
 
         else:
@@ -31,7 +51,7 @@ class Page(object):
     header = '''<!DOCTYPE>
 <html>
     <head>
-        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <link rel="stylesheet" href="style/style.css" type="text/css" />
         <title>LAB: Simple Form</title>
     </head>
     <body>
@@ -39,14 +59,22 @@ class Page(object):
     content = '''Hello There'''
     form_content = '''
         <form method="GET">
-            <h4>First Name</h4>
+        <div id= "wrapper">
+            <h4>Sign Up</h4>
             <input type="text" placeholder="First Name" name= "fname" />
-            <h4>Last Name</h4>
+            <br/>
             <input type="text" placeholder="Last Name" name= "lname"  />
+            <br/>
+            <input type="text" placeholder="Email Address" name= "email"  />
+            <br/>
+            <input type="password" placeholder="Enter Password" name= "pass"  />
+            <br/>
+            <input type="text" placeholder="Re-Type Password" name= "repass"  />
+            <br/>
 
 
         <div id ="dropbox" class="float_left clear">
-        <h4>Newsletter: <br>Would You like to receive Newsletters via Email</h4>
+        <h4>Newsletter: <br> Would You like to receive Newsletters via Email</h4>
         <select name = "marketing">
             <option> Select One</option>
             <option value = "week" name = "week">Yes, Weekly</option>
@@ -56,24 +84,15 @@ class Page(object):
         </select>
 
         <h4>How Would you describe yourself (check all that may apply)</h4>
-            <input type="checkbox"  name= "artistic">Artistic</input>
-            <input type="checkbox"  name= "athletic">Athletic</input>
-            <input type="checkbox"  name= "music">Music Snob</input>
-            <input type="checkbox"  name= "food">Foodie</input>
-            <input type="checkbox"  name= "athletic"></input>
-
-
-
-
-
-
-
-
+            <input type="checkbox"  name= "artistic" value ="Artistic">Artistic<br/>
+            <input type="checkbox"  name= "athletic" value ="Athletic">Athletic<br/>
+            <input type="checkbox"  name= "music" value ="Music">Music Snob<br/>
+            <input type="checkbox"  name= "food" value ="Foodie">Foodie
 
             </div>
 
-    <input type="submit" value="submit info" />
-
+            <input type="submit" value="submit info" />
+        <div>
             </div>
 </form>
     '''
