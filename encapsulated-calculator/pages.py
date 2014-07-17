7d181f9b456bed481043a625405e48cad5a2e26c
ctrovato <carminetrovato@fullsail.edu>
2014-07-17 01:58:31 -0400

    Encapulated Calc

---
+++ encapsulated-calculator/pages.py
@@ -0,0 +1,17 @@
+'''
+Carmine Trovato
+July 15th 2014
+DPW 1407 Encapsulated Calculator
+'''
+
+
+
+class Television(object):
+    def __init__(self):
+        self.__television = ""
+        self.__price = 0
+        self.__sound = 0
+        self.__bluray = 0
+        self.__warranty = 0
+        self.__total = 0
+        self.__open = '''
